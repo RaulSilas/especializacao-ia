8f08e9f0f41dbb1ff215964cd08c69c5c9ee3038
Raul Silas <raulsilas22@gmail.com>
2026-06-23 11:45:43 -0300

    tokenization e exemplos usando o tf-idf

---
+++ tokenization-02.py
@@ -0,0 +1,54 @@
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+documents = [
+    "Machine learning é um campo da inteligência artificial que permite que computadores aprendam padrões a partir de dados.",
+    "O aprendizado de máquina dá aos sistemas a capacidade de melhorar seu desempenho sem serem explicitamente programados.",
+    "Em vez de seguir apenas regras fixas, o machine learning descobre relações escondidas nos dados.",
+    "Esse campo combina estatística, algoritmos e poder computacional para extrair conhecimento.",
+    "O objetivo é criar modelos capazes de generalizar além dos exemplos vistos no treinamento.",
+    "Aplicações de machine learning vão desde recomendações de filmes até diagnósticos médicos.",
+    "Os algoritmos de aprendizado de máquina transformam dados brutos em previsões úteis.",
+    "Diferente de um software tradicional, o ML adapta-se conforme novos dados chegam.",
+    "O aprendizado pode ser supervisionado, não supervisionado ou por reforço, dependendo do tipo de problema.",
+    "Na prática, machine learning é o motor que impulsiona muitos avanços em visão computacional e processamento de linguagem natural.",
+    "Mais do que encontrar padrões, o machine learning ajuda a tomar decisões baseadas em evidências.",
+]
+
+
+def preprocess(text):
+    text_lower = text.lower()
+
+    tokens = nltk.word_tokenize(text_lower)
+
+    # Esse trecho do código retorna todos os tokens (palavras) que contenham apenas letras e números
+    # Eliminando pontuação, letras especiais e outras coisas que podem gerar ruidos
+    return [word for word in tokens if word.isalnum()]
+
+
+# E esse trecho reune os tokens em um string ao texto original, porém sem pontuação, caracteres especiais e etc.
+preprocessed_docs = [" ".join(preprocess(doc)) for doc in documents]
+
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(preprocessed_docs)
+
+query = "machine learning"
+
+
+def search_tfidf(query, vectorizer, tfidf_matrix):
+    query_vector = vectorizer.transform([query])
+    similarities = cosine_similarity(tfidf_matrix, query_vector).flatten()
+    sorted_similarities = list(enumerate(similarities))
+    results = sorted(sorted_similarities, key=lambda x: x[1], reverse=True)
+    return results
+
+
+search_similarities = search_tfidf(query, vectorizer, tfidf_matrix)
+
+print(search_similarities)
+
+print(f"Top 10 documentos por score de similaridade: {query}")
+for doc_index, score in search_similarities[:10]:
+    if score != 0:
+        print(f"Index: {doc_index}, score: {score}")
